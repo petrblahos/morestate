a030def8ff76ffd8b18c8b0828e360673347858e
Petr Blahoš <petr@blahos.com>
2020-07-14 13:59:52 +0200

    Changed the settings file path and name.
    Using path_provider's getApplicationSupportDirectory.

--- lib/backend.dart
+++ lib/backend.dart
@@ -6,17 +6,18 @@ import 'package:path_provider/path_provider.dart';
 import 'package:morestate/model/model.dart';
 
 // https://flutter.dev/docs/cookbook/persistence/reading-writing-files
+// https://pub.dev/documentation/path_provider/latest/path_provider/path_provider-library.html
 
 class LocalModelStorage {
   Future<String> get _localPath async {
-    final directory = await getApplicationDocumentsDirectory();
+    final directory = await getApplicationSupportDirectory();
 
     return directory.path;
   }
 
   Future<File> get _localFile async {
     final path = await _localPath;
-    return File('$path/modelstorage.txt');
+    return File('$path/modelstorage.json');
   }
 
   Future<Model> loadModel() async {
